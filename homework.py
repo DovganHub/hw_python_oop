a5239af5e3f8f05a5c954635890ac424a48bb09c
Dovganyuk <asdovganyuk@gmail.com>
2021-08-05 11:01:46 +0300

    added get_remnant() method, corrected 8 errors

--- homework.py
+++ homework.py
@@ -1,14 +1,18 @@
 import datetime as dt
 from typing import Optional
+DATE_FORMAT = '%d.%m.%Y'
 
 
 class Record:
     def __init__(self, amount: int, comment: str,
                  date: Optional[str] = None) -> None:
         self.amount: int = amount
-        self.date = (dt.datetime.now().date()
-                     if date is None
-                     else dt.datetime.strptime(date, '%d.%m.%Y').date())
+        if date is None:
+            self.date = dt.datetime.now().date()
+        else:
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
+            # Привет! Вынес формат в константу за пределами класса, но ведь
+            # когда класс импортируют, там этой переменной не будет, как быть?
         self.comment: str = comment
 
 
@@ -28,51 +32,52 @@ class Calculator:
         """
         Возвращает расход за текущий день.
         """
-        return sum([i.amount for i in self.records
-                    if i.date == dt.datetime.now().date()])
+        return sum(record.amount for record in self.records
+                   if record.date == dt.datetime.now().date())
 
     def get_week_stats(self) -> float:
         """
         Возвращает сумму amount по всем записям за последнюю неделю
         """
-        start = dt.datetime.now().date() - dt.timedelta(days=7)
         end = dt.datetime.now().date()
-        return sum([i.amount for i in self.records if start <= i.date <= end])
+        start = end - dt.timedelta(days=7)
+        return sum(record.amount for record in self.records
+                   if start <= record.date <= end)
+
+    def get_remnant(self) -> float:
+        """
+        Возвращает остаток amount на текущий момент
+        """
+        return self.limit - self.get_today_stats()
 
 
 class CashCalculator(Calculator):
-    USD_RATE = 60.0
-    EURO_RATE = 70.0
-
-    def __init__(self, limit) -> None:
-        super().__init__(limit)
-
-        self.currency_dict = {'rub': 'руб', 'usd': 'USD', 'eur': 'Euro'}
-        self.rates_dict = {'rub': 1, 'usd': self.USD_RATE,
-                           'eur': self.EURO_RATE}
+    USD_RATE = 300.0
+    EURO_RATE = 500.0
 
     def get_today_cash_remained(self, currency) -> str:
         """
         Возвращает остаток денег в заданной валюте на текущий день.
         """
-        remnant = self.limit - self.get_today_stats()
-        remnant = round(remnant / self.rates_dict[currency], 2)
-
-        if remnant > 0:
-            return (f'На сегодня осталось {remnant}'
-                    + f' {self.currency_dict[currency]}')
-        elif remnant == 0:
+        currency_dict = {'rub': ('руб', 1), 'usd': ('USD', self.USD_RATE),
+                         'eur': ('Euro', self.EURO_RATE)}
+        remnant = round(self.get_remnant() / currency_dict[currency][1], 2)
+        currency_text = currency_dict[currency][0]
+        if remnant == 0:
             return 'Денег нет, держись'
+        elif remnant > 0:
+            return (f'На сегодня осталось {remnant}'
+                    f' {currency_text}')
         else:
             return (f'Денег нет, держись: твой долг - {abs(remnant)}'
-                    + f' {self.currency_dict[currency]}')
+                    f' {currency_text}')
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self) -> str:
-        remnant = self.limit - self.get_today_stats()
+        remnant = self.get_remnant()
         if remnant <= 0:
             return 'Хватит есть!'
-        else:
-            return ('Сегодня можно съесть что-нибудь ещё, но'
-                    + f' с общей калорийностью не более {remnant} кКал')
+        # else был избыточен, т.к. ничего не менял
+        return ('Сегодня можно съесть что-нибудь ещё, но'
+                f' с общей калорийностью не более {remnant} кКал')
